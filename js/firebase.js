09cae2f183035c7f23fead0c2f8251bdecc2611f
dkrnahs515-stack <dkrnahs515@gmail.com>
2026-06-02 11:55:21 +0900

    Load copy previous report feature

--- js/firebase.js
+++ js/firebase.js
@@ -7,6 +7,7 @@
    ============================================================ */
 
 import './print-layout.js';
+import './copy-prev-to-next.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, get, remove, onValue, child }
   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
